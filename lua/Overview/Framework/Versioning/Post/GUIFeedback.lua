dd3e13aab55ff0f126f4e074580b978a445cb9b7
adsfgg <11364579+adsfgg@users.noreply.github.com>
2019-06-28 03:55:23 +0100

    update to latest framework

---
+++ lua/Overview/Framework/Versioning/Post/GUIFeedback.lua
@@ -0,0 +1,13 @@
+local Mod = GetMod()
+
+if Mod:GetConfig().kShowInFeedbackText then
+
+	local originalFeedbackInit
+
+	originalFeedbackInit = Class_ReplaceMethod("GUIFeedback", "Initialize",
+	function(self)
+		originalFeedbackInit(self)
+		self.buildText:SetText(self.buildText:GetText() .. Mod.Versioning:GetFeedbackText())
+	end)
+
+end
