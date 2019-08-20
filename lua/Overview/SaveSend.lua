67e12d6558ae626d42dfc851c60061d625a287ef
adsfgg <11364579+adsfgg@users.noreply.github.com>
2019-08-20 14:59:26 +0100

    remove libraries and add chat messages for error codes

--- lua/Overview/SaveSend.lua
+++ lua/Overview/SaveSend.lua
@@ -3,50 +3,49 @@ Script.Load("lua/Overview/LibDeflate.lua")
 local LibDeflate = GetLibDeflate()
 local ns2OverviewStatsURL = "https://overview.4sdf.co.uk/receiveRoundData"
 
-local function SendData(compressedJsonData)
-    if not Server.IsDedicated() then
-        print("Overview: Not sending data for local rounds.")
-        return
-    end
+local function SendData(jsonData, SendChatMessage)
+    local status = -128
+    local reason = "No response."
 
-    Shared.SendHTTPRequest( ns2OverviewStatsURL, "POST", { data = compressedJsonData }, function(response)
+    Shared.SendHTTPRequest( ns2OverviewStatsURL, "POST", { data = jsonData }, function(response)
         local data, pos, err = json.decode(response)
 
         if err then
             Shared.Message("Could not parse NS2 overview response. Error: " .. ToString(err) .. ". Response: " .. response)
+            status, reason  = -1, "Could not parse NS2 Overview response."
+        else
+            status = data['status']
+            reason = data['reason']
+
+            if status ~= 0 then
+                Shared.Message("Overview: Status - " .. status)
+                Shared.Message("Overview: Reason - " .. reason)
+            end
         end
+
+        if status ~= 0 then
+            SendChatMessage("Demo failed to upload.")
+            SendChatMessage("Status: " .. status)
+            SendChatMessage("Reason: " .. reason)
+        else
+            -- notify the players that the demo was saved successfully.
+            SendChatMessage("Demo recorded.")
+        end
+
     end)
 end
 
-local function SaveData(jsonData, compressedJsonData)
+local function SaveData(jsonData)
     local dataFile = io.open("config://RoundStats.json", "w+")
-    local cfile = io.open("config://RoundStatsCompressed.txt", "w+")
 
     if dataFile then
         dataFile:write(jsonData)
         io.close(dataFile)
     end
-
-    if cfile then
-        cfile:write(compressedJsonData)
-        io.close(cfile)
-    end
 end
 
-function SaveAndSendRoundData(jsonStructure)
+function SaveAndSendRoundData(jsonStructure, SendChatMessage)
     local jsonData = json.encode(jsonStructure, { indent=true })
-    local now = Shared.GetTime()
-    local compressedJsonData = LibDeflate:CompressDeflate(json.encode(jsonStructure, { indent=false }))
-    local timeTaken = Shared.GetTime() - now
-
-    print("Compression took: " .. timeTaken .. "ms")
-
-    now = Shared.GetTime()
-    SaveData(jsonData, compressedJsonData)
-    timeTaken = Shared.GetTime() - now
-    print("saving regular file took: " .. timeTaken)
-    now = Shared.GetTime()
-    SendData(compressedJsonData)
-    timeTaken = Shared.GetTime() - now
-    print("saving compressed file took: " .. timeTaken)
+    SaveData(jsonData)
+    SendData(jsonData, SendChatMessage)
 end
