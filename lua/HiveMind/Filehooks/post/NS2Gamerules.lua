ba0b02950a052424036363e66d1aa802c4cd69f2
adsfgg <11364579+adsfgg@users.noreply.github.com>
2019-08-30 10:29:53 +0100

    remove obsolete filehook

--- lua/HiveMind/Filehooks/post/NS2Gamerules.lua
+++
@@ -1,8 +0,0 @@
-if Server then
-    local oldOnCreate = NS2Gamerules.OnCreate
-    function NS2Gamerules:OnCreate()
-        oldOnCreate(self)
-        StatsTracker:SetupTeamHandlers(self.team1)
-        StatsTracker:SetupTeamHandlers(self.team2)
-    end
-end
